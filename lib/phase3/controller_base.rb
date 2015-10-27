afcba8e38a95078b362d3cfe9472456874fd8ea0
pmbrent <patriciambrent@gmail.com>
2015-10-27 14:02:52 -0400

    Render ERB

--- lib/phase3/controller_base.rb
+++ lib/phase3/controller_base.rb
@@ -1,5 +1,6 @@
 require_relative '../phase2/controller_base'
 require 'active_support'
+require 'active_support/inflector'
 require 'active_support/core_ext'
 require 'erb'
 
@@ -8,6 +9,13 @@ module Phase3
     # use ERB and binding to evaluate templates
     # pass the rendered html to render_content
     def render(template_name)
+      controller_name = self.class.to_s.underscore
+
+      temp = File.read "views/#{controller_name}/#{template_name}.html.erb"
+
+      the_result = ERB.new(temp).result(binding)
+
+      render_content(the_result, "text/html")
     end
   end
 end
