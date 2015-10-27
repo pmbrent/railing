f1a97b683e90d33de81321323d3ec1687e21fcfd
pmbrent <patriciambrent@gmail.com>
2015-10-27 15:53:16 -0400

    Parse nested hash from params

--- lib/phase5/params.rb
+++ lib/phase5/params.rb
@@ -10,9 +10,11 @@ module Phase5
     # You haven't done routing yet; but assume route params will be
     # passed in as a hash to `Params.new` as below:
     def initialize(req, route_params = {})
+      @params = parse_www_encoded_form(req.query_string)
     end
 
     def [](key)
+      @params[key]
     end
 
     # this will be useful if we want to `puts params` in the server log
@@ -29,11 +31,43 @@ module Phase5
     # should return
     # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
     def parse_www_encoded_form(www_encoded_form)
+      once_decoded = URI::decode_www_form(www_encoded_form)
+
+      decoded = Hash.new
+
+      once_decoded.each do |key, value|
+        nest = parse_key(key)
+
+        decoded.merge insert_value(decoded, nest, value)
+
+      end
+      decoded
+    end
+
+    def insert_value(hash, key_list, value)
+      return value if key_list.size == 0
+
+      if hash.has_key?(key_list[0])
+        hash[key_list[0]].merge insert_value(hash[key_list[0]], key_list[1..-1], value)
+      else
+        temp = value
+        key_list.reverse.each do |k|
+          temp = { k => temp }
+        end
+        hash[temp.keys.first] = temp.values.first
+        return hash
+      end
+
     end
 
     # this should return an array
     # user[address][street] should return ['user', 'address', 'street']
     def parse_key(key)
+      splitkeys = key.split(/\]\[|\[|\]/)
+      return key if splitkeys.size == 1
+      splitkeys.map do |new_key|
+        parse_key(new_key)
+      end
     end
   end
 end
