5c66451c639a4ec2ed82a7180564903e188856aa
pmbrent <patriciambrent@gmail.com>
2015-10-28 12:01:28 -0400

    Storage/Retrieval of Flash class

--- lib/phase7/flash.rb
+++ lib/phase7/flash.rb
@@ -5,34 +5,32 @@ class Flash
 
   attr_reader :new_values, :now_values
 
-  def initialize(req, res)
+  def initialize(req)
     @req = req
-    @res = res
-    @cookie = WEBrick::Cookie.new("_rails_lite_flash", "")
-    @cookie.path = '/'
     @new_values = {}
     @now_values = {}
 
-    if req.nil?
-      existing_cookie = nil
-    else
-      existing_cookie = req.cookies.find do |cookie|
-        cookie.name == '_rails_lite_app'
-      end
+    existing_cookie = req.cookies.find do |cookie|
+      cookie.name == "_rails_lite_flash"
     end
 
     unless existing_cookie.nil?
-       @now_values = JSON.parse(existing_cookie.value)
+      @now_values = JSON.parse(existing_cookie.value)
     end
 
   end
 
-  def []=(key, value)
-    @new_values[key] = value
+  def []=(key, val)
+    @new_values[key] = val
   end
 
   def [](key)
-    @now_values[key].concat(@new_values[key])  #assumes they're arrays
+    if !@now_values.empty? && !@new_values.empty?
+      @now_values[key].concat(@new_values[key])  #assumes they're arrays
+    else
+      @now_values[key] if @now_values
+      @new_values[key] if @new_values
+    end
   end
 
   def now
@@ -40,8 +38,10 @@ class Flash
     @now_values = @now_values.merge (temp.new_values)
   end
 
-  def save
-    @new_values #only these are saved back for next session
+  def store_flash(res)
+    cookie = WEBrick::Cookie.new("_rails_lite_flash", @new_values.to_json)
+    cookie.path = '/'
+    res.cookies << cookie
   end
 
 end
